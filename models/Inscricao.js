9720e0de0f5eae51fbc35009716ebafad80d5d3c
Rayani Gomes <rayanigomesrn100@gmail.com>
2025-02-28 17:08:08 -0300

    Update Inscricao.js

--- models/Inscricao.js
+++ models/Inscricao.js
@@ -23,6 +23,10 @@ const InscricaoSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     email: { type: String, required: true },
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Inscricao", InscricaoSchema);
